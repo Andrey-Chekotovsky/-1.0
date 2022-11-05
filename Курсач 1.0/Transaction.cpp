c511d9b32cbc3940029262f113c065614591a9fe
Andrey Checkotovsky <checkotovskyandrey@gmail.com>
2022-11-05 20:26:04 +0300

    Transaction update

--- Курсач 1.0/Transaction.cpp
+++ Курсач 1.0/Transaction.cpp
@@ -1,1 +1,41 @@
 #include "Transaction.h"
+
+template <class T>
+Transaction<T>::Transaction(T object)
+{
+	data = object;
+	this->previous = NULL;
+	this->next = NULL;
+}
+
+template <class T>
+Transaction<T>* Transaction<T>::Update(T object)
+{
+	Transaction* trans = new Transaction(object);
+	if (this->next != NULL)
+	{
+		this->next.~shared_ptr();
+	}
+	trans->previous = this;
+	this->next = trans;
+	trans->next = NULL;
+	return trans;
+}
+
+template <class T>
+std::shared_ptr<Transaction<T>> Transaction<T>::go_previous_state()
+{
+	if (this->previous != NULL)
+		return this->previous;
+}
+template <class T>
+std::shared_ptr<Transaction<T>> Transaction<T>::go_next_state()
+{
+	if (this->next != NULL)
+		return this->next;
+}
+template <class T>
+T Transaction<T>::get_data()
+{
+	return this->data;
+}
--- Курсач 1.0/Transaction.h
+++ Курсач 1.0/Transaction.h
@@ -4,42 +4,15 @@
 #include <memory>
 
 template <class T>
-class Transaction  // комментарий
+class Transaction  
 {
 	T data;
 	std::shared_ptr<Transaction> previous;
 	std::shared_ptr<Transaction> next;
-	Transaction(T object)
-	{
-		data = object;
-		this->previous = NULL;
-		this->next = NULL;
-	}
-	Transaction* Update(T object)
-	{
-		Transaction* trans = new Transaction(object);
-		if (this->next != NULL)
-		{
-			this->next.~shared_ptr();
-		}
-		trans->previous = this;
-		this->next = trans;
-		trans->next = NULL;
-		return trans;
-	}
-	std::shared_ptr<Transaction> go_previous_state()
-	{
-		if(this->previous != NULL)
-			return this->previous;
-	}
-	std::shared_ptr<Transaction> go_next_state()
-	{
-		if (this->next != NULL)
-			return this->next;
-	}
-	T get_data()
-	{
-		return this->data;
-	}
+	Transaction(T object);
+	Transaction* Update(T object);
+	std::shared_ptr<Transaction> go_previous_state();
+	std::shared_ptr<Transaction> go_next_state();
+	T get_data();
 };
 

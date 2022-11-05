14d1f60d71077eaf275fa0e4acd67306642fa919
Andrey Checkotovsky <checkotovskyandrey@gmail.com>
2022-11-05 20:34:35 +0300

    Transaction update

--- Курсач 1.0/Transaction.h
+++ Курсач 1.0/Transaction.h
@@ -6,9 +6,11 @@
 template <class T>
 class Transaction  
 {
+private:
 	T data;
 	std::shared_ptr<Transaction> previous;
 	std::shared_ptr<Transaction> next;
+public:
 	Transaction(T object);
 	Transaction* Update(T object);
 	std::shared_ptr<Transaction> go_previous_state();
